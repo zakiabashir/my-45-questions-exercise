5810862abc390639083ab85f23eb2d63058282ae
zakiabashir <nshafeys0@gmail.com>
2024-03-23 04:02:10 -0700

    question add

--- question1-45.ts
+++ question1-45.ts
@@ -706,7 +706,13 @@ make_sandwich('turkey', 'bacon');
 make_sandwich('peanut butter', 'jelly');
 
 // question#45
+// Cars: Write a function that stores information about a car in a Object. 
+// The function should always receive a manufacturer and a model name. 
+// It should then accept an arbitrary number of keyword arguments. 
+// Call the function with the required information and two other name-value pairs,
+//  such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
 
+// answer:
 // / Function to store information about a car in an Object
 
 function car_info(manufacturer: string, model: string, ...details: [string, any][]) {
